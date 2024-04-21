e9cb532c8b60fdddeff8ec735d146712e5f05812
Nikita Ganzikov <nganzikov@gmail.com>
2024-04-21 12:23:13 +0300

    feat(test): TestKeyRemove for UserDefaults

--- userdefaults_test.go
+++ userdefaults_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-func TestNewPUserDefaults(t *testing.T) {
+func TestNewUserDefaults(t *testing.T) {
 	defaults := NewUserDefaults()
 	if defaults.userDefaultsPtr == nil {
 		t.Fatalf("pointer to C defaults is nil!")
@@ -13,10 +13,26 @@ func TestNewPUserDefaults(t *testing.T) {
 
 func TestStringValue(t *testing.T) {
 	defaults := NewUserDefaults()
+
 	value := "FooBar!"
 	defaults.SetString("TestKey", value)
+
 	readValue := defaults.GetString("TestKey")
 	if readValue != value {
 		t.Fatalf("read value should be %s, but is %s", value, readValue)
+	}
+}
+
+func TestKeyRemove(t *testing.T) {
+	defaults := NewUserDefaults()
+
+	value := "FooBar!"
+	defaults.SetString("TestKey", value)
+
+	defaults.Remove("TestKey")
+
+	readValue := defaults.GetString("TestKey")
+	if readValue == "" {
+		t.Fatalf("read value should be empty string but got %s", readValue)
 	}
 }
