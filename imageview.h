6d38f07068f81bb3122d039d62046513b506d499
Nikita Ganzikov <nganzikov@gmail.com>
2024-04-21 08:57:53 +0300

    fix: add SetImageUrl in header

--- imageview.h
+++ imageview.h
@@ -5,6 +5,7 @@
 typedef void* ImageViewPtr;
 
 ImageViewPtr ImageView_New(int goImageViewID, int x, int y, int w, int h, const char* url);
+void ImageView_SetImageUrl(ImageViewPtr imageViewPtr, const char* url);
 void ImageView_SetAnimates(ImageViewPtr imageViewPtr, int animates);
 void ImageView_SetContentTintColor(ImageViewPtr imageViewPtr, int r, int g, int b, int a);
 void ImageView_SetEditable(ImageViewPtr imageViewPtr, int editable);
